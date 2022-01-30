e129ef546d821920a676b1de7d0f75850d8bd16d
paritanavadiya <90469290+paritanavadiya@users.noreply.github.com>
2022-01-30 17:22:27 -0500

    Update db.py

--- nutrihealth/db.py
+++ nutrihealth/db.py
@@ -7,6 +7,6 @@ from pymongo import MongoClient
 
 def default_database():
     client = MongoClient(
-        'mongodb+srv://Parita:uQm3T!3dE!g5*!K@cluster0.y79rr.mongodb.net/NutriHelth?retryWrites=true&w=majority')
+        '####')
     db = client['NutriHelth']
     return db
